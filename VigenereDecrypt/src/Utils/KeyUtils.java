0b4ca39b56665fac9a113897c1791aa6bfda863c
Francisco Joao Lucca Neto <franciscojoaoluccaneto@gmail.com>
2022-09-15 15:14:28 -0300

    Adding more code

--- VigenereDecrypt/src/Utils/KeyUtils.java
+++ VigenereDecrypt/src/Utils/KeyUtils.java
@@ -8,7 +8,6 @@ public class KeyUtils {
 
     private static final int MAX_KEY_LENGTH = 30;
     private static final ArrayList<String> keysPt = new ArrayList<>();
-
     private static final ArrayList<String> keysEn = new ArrayList<>();
 
     private static final HashMap<Integer, Double> coincidenceIndexPt = new HashMap<>();
@@ -29,8 +28,33 @@ public class KeyUtils {
     }
 
     public static void findKeys(final String encryptedText) {
+        calculateCoincidenceIndex(encryptedText);
+    }
+
+    private static void calculateCoincidenceIndex(final String encryptedText) {
         for (int keySize = 0; keySize < MAX_KEY_LENGTH; keySize++) {
             ArrayList<String> encryptedSplitText = splitText(encryptedText, keySize);
+
+            ArrayList<HashMap<Character, Integer>> frequencies = calculateFrequency(encryptedSplitText);
+
+            for (final HashMap<Character, Integer> frequency : frequencies) {
+                final long subStringSize = frequency.remove('/');
+                final long n = subStringSize * (subStringSize - 1);
+                long sum = 0;
+
+                for (final char c : frequency.keySet()) {
+                    final long letterFrequency = frequency.get(c);
+                    sum += letterFrequency * (letterFrequency - 1);
+                }
+
+                final double coincidenceIndex = (double) sum / n;
+
+                if (coincidenceIndex > 0.062 && coincidenceIndex < 0.07) {
+                    coincidenceIndexEn.put(keySize, coincidenceIndex);
+                } else if (coincidenceIndex > 0.07 && coincidenceIndex < 0.078) {
+                    coincidenceIndexPt.put(keySize, coincidenceIndex);
+                }
+            }
         }
     }
 
@@ -48,4 +72,27 @@ public class KeyUtils {
 
         return textSplit;
     }
+
+    private static ArrayList<HashMap<Character, Integer>> calculateFrequency(final ArrayList<String> slipText) {
+        final ArrayList<HashMap<Character, Integer>> frequencies = new ArrayList<>();
+        final HashMap<Character, Integer> analysis = new HashMap<>();
+        for (final String aux : slipText) {
+            analysis.clear();
+            analysis.putIfAbsent('/', aux.length());
+
+            for (int i = 0; i < aux.length(); i++) {
+                final char letter = aux.charAt(i);
+                if (analysis.containsKey(letter)) {
+                    analysis.put(letter, analysis.get(letter) + 1);
+                } else {
+                    analysis.putIfAbsent(letter, 1);
+                }
+            }
+            frequencies.add(analysis);
+        }
+
+        return frequencies;
+    }
+
+
 }
