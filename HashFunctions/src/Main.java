bf323719c50e3be5abe3811ae3c94cad30ca6e13
Francisco Joao Lucca Neto <franciscojoaoluccaneto@gmail.com>
2022-10-01 18:43:08 -0300

    Minor fixes. T2 Done

--- HashFunctions/src/Main.java
+++ HashFunctions/src/Main.java
@@ -4,7 +4,6 @@ import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
     private static final String PATH = "./src/Resources/";
@@ -18,64 +17,58 @@ public class Main {
         return Files.readAllBytes(Paths.get(fullPath));
     }
 
-    private static ArrayList<byte[]> divideChunks(final byte[] fileInBytes) {
-        final ArrayList<byte[]> chunks = new ArrayList<>();
+    private static ArrayList<byte[]> splitBytes(final byte[] bytes) {
+        ArrayList<byte[]> bytesChunks = new ArrayList<>();
 
-        for (int i = 0; i < fileInBytes.length; i += BYTE_SIZE) {
-            byte[] fatia = new byte[BYTE_SIZE];
+        for (int i = 0; i < bytes.length; i = i + BYTE_SIZE) {
+            byte[] slice = new byte[BYTE_SIZE];
             for (int j = 0; j < BYTE_SIZE; j++) {
-                if (j + i < fileInBytes.length)
-                    fatia[j] = fileInBytes[j + i];
+                if (j + i < bytes.length)
+                    slice[j] = bytes[j + i];
                 else {
                     sizeOfLastChunk = j;
                     break;
                 }
             }
-            chunks.add(fatia);
+            bytesChunks.add(slice);
         }
-
-        return chunks;
+        return bytesChunks;
     }
 
-    private static byte[] findFirstHash(final ArrayList<byte[]> chunks, final MessageDigest digest) {
+    private static byte[] getH0(final ArrayList<byte[]> divBytes) throws NoSuchAlgorithmException {
+        byte[] lastChunk = divBytes.remove(divBytes.size() - 1);
+        byte[] lastBlock = new byte[sizeOfLastChunk];
+        System.arraycopy(lastChunk, 0, lastBlock, 0, lastBlock.length);
+
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        digest.update(lastBlock);
+
         byte[] hash = digest.digest();
 
-        for (int i = chunks.size() - 1; i >= 0; i--) {
-            byte[] bloco = chunks.get(i);
+        for (int i = divBytes.size() - 1; i >= 0; i--) {
+            byte[] bloco = divBytes.get(i);
             byte[] toHash = new byte[hash.length + bloco.length];
             System.arraycopy(bloco, 0, toHash, 0, bloco.length);
             System.arraycopy(hash, 0, toHash, bloco.length, hash.length);
             digest.update(toHash);
             hash = digest.digest();
         }
-
         return hash;
     }
 
     public static void main(String[] args) {
-        final Scanner in = new Scanner(System.in);
-        System.out.println("Witch file do you want?");
-        final String fileName = in.nextLine();
         try {
-            final byte[] fileInBytes = readFile(fileName);
-            final ArrayList<byte[]> chunks = divideChunks(fileInBytes);
-            final byte[] lastChunk = chunks.remove(chunks.size() - 1);
-            final byte[] copyLastChunk = new byte[sizeOfLastChunk];
-            System.arraycopy(lastChunk, 0, copyLastChunk, 0, copyLastChunk.length);
+            byte[] bytes = readFile("FuncoesResumo - SHA1.mp4");
+            ArrayList<byte[]> bytesSlices = splitBytes(bytes);
+            final byte[] hashAnterior = getH0(bytesSlices);
 
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            digest.update(lastChunk);
-
-            byte[] hash = findFirstHash(chunks, digest);
-
-            StringBuilder builder = new StringBuilder(hash.length * 2);
-            for (byte b : hash)
-                builder.append(String.format("%02x", b));
-            System.out.println("Found h0:");
-            System.out.println(builder);
+            StringBuilder sb = new StringBuilder(hashAnterior.length * 2);
+            for (byte b : hashAnterior)
+                sb.append(String.format("%02x", b));
+            System.out.println("Found H0:");
+            System.out.println(sb);
         } catch (IOException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-
     }
 }
